66e9454a2295cfa50faf1a79facfcc71adeafa0e
Edy <acclarolabs@gmail.com>
2026-06-22 21:15:35 -0600

    Update export name to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   // 1. Crear una respuesta inicial
   let response = NextResponse.next({
     request: {
